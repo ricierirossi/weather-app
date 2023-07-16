b3ecd19f7df8eb384b4ef596e34e70d59180d95d
ricirossi <matheus.ricieri@hotmail.com>
2023-07-16 20:03:21 -0300

    Add Sunrise card

---
+++ src/components/SunRise.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="highlight-card">
+        <div>Sunrise</div>
+        <div>{{ sunrise }}</div>
+        <div>Extra</div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['sunrise']
+}
+</script>
+
+<style>
+.highlight-card {
+    background-color: brown;
+}
+</style>
