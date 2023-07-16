36aa49fdb52da53e8ab1cc8ed73a9c80f3cc2f9a
ricirossi <matheus.ricieri@hotmail.com>
2023-07-16 20:03:13 -0300

    Add Sunset card

---
+++ src/components/SunSet.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="highlight-card">
+        <div>Sunset</div>
+        <div>{{ sunset }}</div>
+        <div>Extra</div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['sunset']
+}
+</script>
+
+<style>
+.highlight-card {
+    background-color: brown;
+}
+</style>
