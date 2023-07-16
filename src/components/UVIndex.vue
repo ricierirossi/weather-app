912f7dd6d4d46aa7e286fd8b767b38d95f4dcdd6
ricirossi <matheus.ricieri@hotmail.com>
2023-07-16 20:03:05 -0300

    Add UVIndex card

---
+++ src/components/UVIndex.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="highlight-card">
+        <div>UV Index</div>
+        <div>{{ uVIndex }}</div>
+        <div>Extra</div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['uVIndex']
+}
+</script>
+
+<style>
+.highlight-card {
+    background-color: brown;
+}
+</style>
