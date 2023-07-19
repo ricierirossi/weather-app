3ee4e968d91b9009fad8b0bc372bc501ae4ee120
ricirossi <matheus.ricieri@hotmail.com>
2023-07-19 16:05:01 -0300

    Start converting sunrise iso8601 time to hh:mm format

--- src/components/SunRise.vue
+++ src/components/SunRise.vue
@@ -8,7 +8,21 @@
 
 <script>
 export default {
-    props: ['sunrise']
+    props: ['sunrise'],
+    data: function () {
+        return {
+            sunriseTime: null
+        }
+    },
+    methods: {
+        getSunrise(time) {
+            const moment = new Date(time)
+            console.log(moment.toLocaleTimeString('en-US'))
+        }
+    },
+    mounted() {
+        this.getSunrise('2023-07-19T05:57')
+    }
 }
 </script>
 
