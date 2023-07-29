033d91a96c4d190df42c40536499a7aaaf53982d
ricirossi <matheus.ricieri@hotmail.com>
2023-07-29 15:35:11 -0300

    Add function to choose city and open/close menu

--- src/components/AsideMenu.vue
+++ src/components/AsideMenu.vue
@@ -1,41 +1,58 @@
 <template>
     <aside class="menu">
+        <div @click="openCloseMenu">x</div>
         <div class="buttons">
-            <SearchButton />
-            <LocalButton />
-            <div class="city">São Paulo</div>
-            <div class="city">London</div>
-            <div class="city">Moscow</div>
+            <SearchLocation />
+            <SearchCity />
+        </div>
+        <div class="cities">
+            <div
+                class="city"
+                :city="'saoPaulo'"
+                :lat="-23.55052"
+                long="-46.633309"
+                @click="chooseCity"
+            >
+                São Paulo
+            </div>
+            <div
+                class="city"
+                :city="'london'"
+                :lat="51.507351"
+                long="-0.127758"
+                @click="chooseCity"
+            >
+                London
+            </div>
+            <div class="city" :city="'moscow'" :lat="55.755826" long="37.6173" @click="chooseCity">
+                Moscow
+            </div>
         </div>
     </aside>
 </template>
 
 <script>
-import SearchButton from './SearchButton.vue'
-import LocalButton from './LocalButton.vue'
+import SearchLocation from './SearchLocation.vue'
+import SearchCity from './SearchCity.vue'
 export default {
-    props: ['temperature', 'weather', 'cityName', 'time'],
-    components: { SearchButton, LocalButton },
-    data: function () {
-        return {
-            today: null
-        }
-    },
+    components: { SearchLocation, SearchCity },
     methods: {
-        formatDate(inputDate) {
-            const date = new Date(inputDate)
-            const options = { weekday: 'short', day: 'numeric', month: 'short' }
-            this.today = date.toLocaleString('en-US', options)
+        chooseCity(e) {
+            const lat = e.target.getAttribute('lat')
+            const long = e.target.getAttribute('long')
+            this.$emit('choose-city', lat, long)
+        },
+        openCloseMenu() {
+            this.$emit('open-close-menu')
         }
-    },
-    beforeUpdate() {
-        this.formatDate(this.time)
     }
 }
 </script>
 
 <style>
 .menu {
+    display: flex;
+    flex-direction: column;
     background-color: #1e213a;
 }
 
@@ -45,7 +62,7 @@ export default {
     margin-top: 40px;
 }
 
-.today {
+.cities {
     display: flex;
     flex-direction: column;
 }
