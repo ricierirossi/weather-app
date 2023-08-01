edada0450def0f2e9de45237ee75fba00e2eb52d
ricirossi <matheus.ricieri@hotmail.com>
2023-08-01 13:55:42 -0300

    Add submitCity method and form
    This method passes the desired city name to AsideMenu component

--- src/components/SearchLocation.vue
+++ src/components/SearchLocation.vue
@@ -1,9 +1,29 @@
 <template>
-    <input placeholder="search location" />
+    <form @submit.prevent="submitCity">
+        <input placeholder="search location" type="text" v-model="city" />
+        <input class="submit-button" type="submit" />
+    </form>
 </template>
 
 <script>
-export default {}
+export default {
+    props: ['searchedLocation'],
+    data: function () {
+        return {
+            city: ''
+        }
+    },
+    methods: {
+        submitCity() {
+            const city = this.city
+            this.$emit('submit-city', city)
+        }
+    }
+}
 </script>
 
-<style></style>
+<style>
+.submit-button {
+    all: unset;
+}
+</style>
