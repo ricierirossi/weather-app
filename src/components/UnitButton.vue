9030fffb3bbf291dc243d1781e563f15cf903cd7
ricirossi <matheus.ricieri@hotmail.com>
2023-08-08 11:51:50 -0300

    Creat UnitButton.vue
    This component replaces both CelsiusButton and FahrenheitButton.

---
+++ src/components/UnitButton.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <span>{{ unit }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['unit', 'selected']
+}
+</script>
+
+<style scoped>
+.selected-unit {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background-color: #e7e7eb;
+    font-size: 18px;
+    font-weight: 700;
+    color: #110e3c;
+}
+
+.unselected-unit {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background-color: #585676;
+    font-size: 18px;
+    font-weight: 700;
+    color: #e7e7eb;
+}
+
+.celsius:hover {
+    cursor: pointer;
+}
+</style>
